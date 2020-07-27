172c749fb16ee07d76d497ea3214a43e66cbd840
Henrymon342 <https://github.com/henrymon342>
2020-07-27 14:49:51 -0400

    contorno de imagen

--- contornos/script.js
+++ contornos/script.js
@@ -7,54 +7,7 @@ original.load("chica_lentes.jpg", function(){
    original.draw(canvas);
 });
 
-function grayScale(){
-	image = original.clone();
-  Marvin.grayScale(original, image);
-  image.draw(canvas);
-}
 
-function blackAndWhite(){
-	image = original.clone();
-  Marvin.blackAndWhite(original, image, 20);
-  image.draw(canvas);
-}
-
-function thresholding(){
-	image = original.clone();
-  Marvin.thresholding(original, image, 160);
-  image.draw(canvas);
-}
-
-function sepia(){
-  image = original.clone();
-  Marvin.sepia(original, image, 30);
-  image.draw(canvas);
-}
-
-function emboss(){
-  image = original.clone();
-  Marvin.emboss(original, image, 30);
-  image.draw(canvas);
-}
-
-function halftone(){
-  image = original.clone();
-	Marvin.halftoneErrorDiffusion(original, image);
-  image.draw(canvas);
-}
-
-function invert(){
-	image = original.clone();
-	Marvin.invertColors(original, image);
-  image.draw(canvas);
-}
-
-function edgeDetection1(){
-	image = original.clone();
-  image.clear(0xFF000000);
-	Marvin.prewitt(original, image);
-	image.draw(canvas);
-}
 
 function edgeDetection2(){
 	image = original.clone();
@@ -65,21 +18,5 @@ function edgeDetection2(){
 	image.draw(canvas);
 }
 
-function crop(){
-  image = original.clone();
-	Marvin.crop(original, image, 140, 165, 125, 50);
-	canvas.getContext("2d").clearRect(0,0,canvas.width, canvas.height);
-	image.draw(canvas);
-}
 
-function scale(){
-	image = original.clone();
-	Marvin.scale(original, image, 200);
-	canvas.getContext("2d").clearRect(0,0,canvas.width, canvas.height);
-	image.draw(canvas);
-}
-
-function reset(){
-	original.draw(canvas);
-}
 
